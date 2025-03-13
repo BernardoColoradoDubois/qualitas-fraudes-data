1e1abf4a33d8d1fe1252e80862d53d29261c286c
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-13 11:25:36 -0600

    ajustando modelos

---
+++ modelos/OFICINAS/DM_OFICINAS.sql
@@ -0,0 +1,4 @@
+--CREATE OR REPLACE TABLE `DM_FRAUDES.DM_OFICINAS`
+SELECT 
+* 
+FROM `qualitasfraude.sample_landing_siniestros_bsc.tsuc_bsc`
---
+++ modelos/PROVEEDORES/DM_PROVEEDORES.sql
@@ -0,0 +1,4 @@
+--CREATE OR REPLACE TABLE `DM_FRAUDES.DM_PROVEEDORES`
+SELECT 
+* 
+FROM `qualitasfraude.sample_landing_siniestros_bsc.prestadores`
