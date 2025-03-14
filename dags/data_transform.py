67c1601a666f7bae56813c319320b00803477247
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-14 14:16:19 -0600

    creando funciones para trabajar con python y bigquery

--- dags/data_transform.py
+++ dags/data_transform.py
@@ -1,58 +1,46 @@
+import airflow
 from airflow import DAG
-from airflow.providers.google.cloud.operators.bigquery import BigQueryOperator
-from datetime import datetime, timedelta
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+from datetime import timedelta
+from airflow.operators.python import PythonOperator
+from lib.utils import execute_query_workflow,execute_query_to_load_database
+import json
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'email_on_failure': False,
-    'email_on_retry': False,
+    'start_date': airflow.utils.dates.days_ago(0),
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(minutes=1)
 }
 
-with DAG(
+dag = DAG(
     'data_transform',
     default_args=default_args,
-    description='Crea tabla DM_ETIQUETA_SINIESTRO con versionamiento SCD-2',
-    schedule_interval=timedelta(days=1),  # Ajusta según tus necesidades
-    start_date=datetime(2023, 1, 1),
+    description='liveness monitoring dag',
+    schedule_interval='0 0 1 1 *',
+    max_active_runs=2,
     catchup=False,
-) as dag:
+    dagrun_timeout=timedelta(minutes=10),
+)
 
-    crear_dm_etiqueta_siniestro = BigQueryOperator(
-        task_id='crear_dm_etiqueta_siniestro',
-        sql='''
-        CREATE OR REPLACE TABLE `DM_FRAUDES.DM_ETIQUETA_SINIESTRO` AS 
-        SELECT 
-          E1.Z_ID AS ID 
-          ,E1.REPORTE AS ID_REPORTE 
-          ,E1.SINIESTRO AS ID_SINIESTRO 
-          ,E1.COD_STATUS AS CODIGO_ESTATUS 
-          ,E1.FEC_INV AS FECHA_INVESTIGACION 
-          ,E1.TEXT_INVEST AS TEXTO_INVESTIGACION 
-          ,E1.USUARIO_INV AS USUARIO_INVESTIGACION 
-          ,E1.TEXT_LOC AS TEXTO_LOCALIZADO 
-          ,E1.FEC_LOC AS FECHA_LOCALIZADO 
-          ,E1.FEC_CARGA AS FECHA_CARGA 
-          ,E1.HORA_PROCESO FECHA_HORA_PROCESO 
-          ,E1.BATCHDATE AS FECHA_LOTE 
-          ,E1.SYSUSERID AS ID_USUARIO_SISTEMA 
-          ,E1.CONSECUTIVO AS CONSECUTIVO 
-          ,E1.FEC_CARGA AS VALIDO_DESDE 
-          ,CASE  
-            WHEN E2.FEC_CARGA IS NOT NULL THEN E2.FEC_CARGA  
-            ELSE TIMESTAMP('9999-12-31 23:59:59') 
-          END AS VALIDO_HASTA 
-          ,CASE  
-            WHEN E2.FEC_CARGA IS NOT NULL THEN FALSE 
-            ELSE TRUE 
-          END AS VALIDO 
-        FROM `STG_FRAUDES.STG_ETIQUETA_SINIESTRO_3` AS E1 
-        LEFT JOIN `STG_FRAUDES.STG_ETIQUETA_SINIESTRO_3` AS E2 
-        ON E1.REPORTE = E2.REPORTE AND E1.CONSECUTIVO+1 = E2.CONSECUTIVO 
-        ORDER BY E1.Z_ID, E1.CONSECUTIVO 
-        ''',
-        use_legacy_sql=False,  # Usa SQL estándar de BigQuery (no Legacy SQL)
-        location='US',  # Ajusta según la ubicación de tu dataset en BigQuery
-    )
+sample_query_workflow = PythonOperator( 
+  task_id='sample_query_workflow', 
+  python_callable=execute_query_workflow, 
+  op_kwargs={ 
+    'project_id': 'qualitasfraude', 
+    'query': "CREATE OR REPLACE TABLE `SAMPLE.SAMPLE` AS SELECT * FROM `qualitasfraude.sample_landing_siniestros.sas_sinies`;"
+  }, 
+  dag=dag 
+)
+
+sample_query = PythonOperator( 
+  task_id='sample_query', 
+  python_callable=execute_query_to_load_database, 
+  op_kwargs={ 
+    'project_id': 'qualitasfraude', 
+    'query': "SELECT * FROM `qualitasfraude.sample_landing_siniestros.sas_sinies` LIMIT 100;"
+  }, 
+  dag=dag 
+)
+
+sample_query_workflow >> sample_query
