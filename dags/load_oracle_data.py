dc228628fd358c7c2565006fa2f3abbd2b129e2f
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-18 14:49:53 -0600

    creando DAG de carga para ORACLE

--- dags/load_oracle_data.py
+++ dags/load_oracle_data.py
@@ -4,8 +4,11 @@ from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 from datetime import timedelta
 from airflow.operators.python import PythonOperator
-from lib.utils import ,get_bucket_file_contents
+from lib.utils import execute_query_to_load_oracle_database,get_bucket_file_contents
+from airflow.providers.oracle.hooks.oracle import OracleHook
 import json
+
+oracle_hook = OracleHook(oracle_conn_id='oracle_conn_id')
 
 default_args = {
     'start_date': airflow.utils.dates.days_ago(0),
@@ -14,11 +17,21 @@ default_args = {
 }
 
 dag = DAG(
-    'fraudes_pipeline',
+    'load_oracle_data',
     default_args=default_args,
     description='liveness monitoring dag',
     schedule_interval='0 0 1 1 *',
     max_active_runs=2,
     catchup=False,
     dagrun_timeout=timedelta(minutes=10),
+)
+
+sample_query = PythonOperator( 
+  task_id='sample_query', 
+  python_callable=execute_query_to_load_oracle_database, 
+  op_kwargs={ 
+    'project_id': 'qualitasfraude', 
+    'query': "SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_CAUSAS`;"
+  }, 
+  dag=dag 
 )
