c650986f530595bd99a0fd51f76862867a94c4a2
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-23 13:34:09 -0600

    creando script de carga de datos prueba exitosa

--- bigquery_to_oracle_script.py
+++ bigquery_to_oracle_script.py
@@ -6,53 +6,44 @@ from google.cloud.bigquery import Client
 from sqlalchemy import create_engine
 import pandas as pd
 import oracledb
+import cx_Oracle
 
 
 # Cargamos las variables de entorno desde el archivo .env
 load_dotenv()
 
-# Accedemos a las variables de entorno
+# Accedemos a las variables de entorno de google
 key_file_path = os.getenv("GCP_LOCAL_JSON_CREDENTIALS_PATH")
+
+# Accedemos a las variables de entorno de oracle
 oracle_user = os.getenv("APPLICATION_ORACLE_USER")
 oracle_password = os.getenv("APPLICATION_ORACLE_PASSWORD")
 oracle_host = os.getenv("APPLICATION_ORACLE_HOST")
 oracle_port = os.getenv("APPLICATION_ORACLE_PORT")
 oracle_service = os.getenv("APPLICATION_ORACLE_SERVICE")
 
+# Inicializamos el cliente de BigQuery
 credentials = GoogleCloudCredentials.from_service_account_file(filename=key_file_path)
-
 client = Client(credentials=credentials)
 
-query = "SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_CAUSAS`;"
-
+# Realizamos una consulta a BigQuery y cargamos los resultados en un DataFrame`
+query = "SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_CAUSAS` WHERE ID IS NOT NULL AND ID <> '*' ORDER BY ID;"
 query_job = client.query(query)
-
 result = query_job.result()
-
 df = result.to_dataframe()
 
-print(df.head())
+dt = [tuple(x) for x in df.values]
+
+#conn_string = f'oracle+cx_oracle://{oracle_user}:{oracle_password}@{oracle_host}:{oracle_port}/{oracle_service}'
+conn_string = f'{oracle_user}/{oracle_password}@{oracle_host}:{oracle_port}/{oracle_service}'
+connection = cx_Oracle.connect(conn_string)
+cursor = connection.cursor()
+cursor.execute('TRUNCATE TABLE INSUMOS.DM_CAUSAS')
+sql='INSERT INTO INSUMOS.DM_CAUSAS VALUES(:1,:2,:3,:4,:5)'
+cursor.executemany(sql, dt)
+connection.commit()
+cursor.close()
 
 
-dsn = f"{oracle_host}:{oracle_port}/{oracle_service}"
-
-connection = oracledb.connect(
-            user=oracle_user,
-            password=oracle_password,
-            dsn=dsn
-        )
 
 
-engine = create_engine(
-            'oracle://', 
-            creator=lambda: connection
-        )
-
-df.to_sql(
-            name='DM_CAUSAS',
-            con=engine,
-            schema='INSUMOS',
-            if_exists='append',  # Append to the table if it exists
-            index=False,
-            chunksize=1000
-        )
