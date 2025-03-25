95ac7a229a0a87dda319c122ec285d7a954dadee
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-24 18:23:50 -0600

    agregando paso intermedio para procesamiento de siniestros

--- dags/fraudes_pipeline.py
+++ dags/fraudes_pipeline.py
@@ -65,6 +65,16 @@ dm_proveedores = PythonOperator(
   dag=dag 
 )
 
+stg_siniestros = PythonOperator( 
+  task_id='stg_siniestros', 
+  python_callable=execute_query_workflow, 
+  op_kwargs={ 
+    'project_id': 'qualitasfraude', 
+    'query': get_bucket_file_contents(path='gs://us-central1-ccompquafrau-38b343aa-bucket/workspaces/models/SINIESTROS/STG_SINIESTROS.sql')
+  }, 
+  dag=dag 
+)
+
 dm_siniestros = PythonOperator( 
   task_id='dm_siniestros', 
   python_callable=execute_query_workflow, 
@@ -147,15 +157,14 @@ dm_pagos_proveedores = PythonOperator(
   dag=dag 
 )
 
-end = BashOperator(task_id='end',bash_command='echo end',dag=dag)
 
 
-init >> dm_causa_cobertura >> end
-init >> dm_causas >> end
-init >> dm_oficinas >> end
-init >> dm_proveedores >> end
-init >> dm_siniestros >> end
-init >> dm_pagos_polizas >> end
-init >> dm_coberturas_movimiento >> end
-init >> dm_pagos_proveedores >> end
-init >> stg_etiqueta_siniestro_1 >> stg_etiqueta_siniestro_2 >> stg_etiqueta_siniestro_3 >> dm_etiqueta_siniestro >> end
+init >> dm_causa_cobertura 
+init >> dm_causas 
+init >> dm_oficinas 
+init >> dm_proveedores 
+init >> stg_siniestros >> dm_siniestros 
+init >> dm_pagos_polizas 
+init >> dm_coberturas_movimiento 
+init >> dm_pagos_proveedores 
+init >> stg_etiqueta_siniestro_1 >> stg_etiqueta_siniestro_2 >> stg_etiqueta_siniestro_3 >> dm_etiqueta_siniestro 
