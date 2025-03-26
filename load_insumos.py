36886b725d2f23f346d0e6b993a40c22ba8f0c38
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-26 12:03:11 -0600

    agregando modulo de password para bcrypt

--- load_insumos.py
+++ load_insumos.py
@@ -8,63 +8,70 @@ from src.lib.bigquery_to_oracle import BigQueryToOracle
 @inject
 def load_dm_causas(bigquery_to_oracle=Provide[DIContainer.bigquery_to_oracle]) -> None:
 
-    bigquery_to_oracle.run(
+    response = bigquery_to_oracle.run(
         extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_CAUSAS` ORDER BY ID", 
         preload_query="TRUNCATE TABLE INSUMOS.DM_CAUSAS",
         schema="INSUMOS",
         table="DM_CAUSAS"
     )    
+    print(response)
+    
 
 @inject
 def load_dm_etiqueta_siniestro(bigquery_to_oracle=Provide[DIContainer.bigquery_to_oracle]) -> None:
 
-    bigquery_to_oracle.run(
+    response = bigquery_to_oracle.run(
         extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_ETIQUETA_SINIESTRO` ORDER BY ID", 
         preload_query="TRUNCATE TABLE INSUMOS.DM_ETIQUETA_SINIESTRO",
         schema="INSUMOS",
         table="DM_ETIQUETA_SINIESTRO"
     )    
+    print(response)
 
 @inject
 def load_dm_oficinas(bigquery_to_oracle=Provide[DIContainer.bigquery_to_oracle]) -> None:
 
-    bigquery_to_oracle.run(
+    response = bigquery_to_oracle.run(
         extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_OFICINAS` ORDER BY ID", 
         preload_query="TRUNCATE TABLE INSUMOS.DM_OFICINAS",
         schema="INSUMOS",
         table="DM_OFICINAS"
     )    
+    print(response)
     
 @inject
 def load_dm_pagos_polizas(bigquery_to_oracle=Provide[DIContainer.bigquery_to_oracle]) -> None:
 
-    bigquery_to_oracle.run(
+    response = bigquery_to_oracle.run(
         extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_PAGOS_POLIZAS` ORDER BY ID", 
         preload_query="TRUNCATE TABLE INSUMOS.DM_PAGOS_POLIZAS",
         schema="INSUMOS",
         table="DM_PAGOS_POLIZAS"
     )   
+    print(response)
 
 
 @inject
 def load_dm_proveedores(bigquery_to_oracle=Provide[DIContainer.bigquery_to_oracle]) -> None:
 
-    bigquery_to_oracle.run(
+    response = bigquery_to_oracle.run(
         extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_PROVEEDORES` ORDER BY ID", 
         preload_query="TRUNCATE TABLE INSUMOS.DM_PROVEEDORES",
         schema="INSUMOS",
         table="DM_PROVEEDORES"
     )   
+    print(response)
     
 @inject
 def load_dm_siniestros(bigquery_to_oracle=Provide[DIContainer.bigquery_to_oracle]) -> None:
 
-    bigquery_to_oracle.run(
-        extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_CAUSAS` ORDER BY ID", 
-        preload_query="TRUNCATE TABLE INSUMOS.DM_CAUSAS",
+    response = bigquery_to_oracle.run(
+        extraction_query="SELECT * FROM `DM_FRAUDES.DM_SINIESTROS` WHERE CAST(FECHA_REGISTRO AS DATE) > '2025-01-01' LIMIT 10000;", 
+        preload_query="TRUNCATE TABLE INSUMOS.DM_SINIESTROS",
         schema="INSUMOS",
-        table="DM_CAUSAS"
+        table="DM_SINIESTROS"
     )   
+    print(response)
 
 load_dotenv()
 
@@ -82,15 +89,6 @@ container = DIContainer()
 container.config.key_file_path.override(key_file_path)
 container.config.connection_string.override(f'{oracle_user}/{oracle_password}@{oracle_host}:{oracle_port}/{oracle_service}')
 container.wire(modules=[__name__])
-
-#big_query_to_oracle = container.big_query_to_oracle()
-
-#big_query_to_oracle.run(
-#    extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_CAUSAS` ORDER BY ID", 
-#    preload_query="TRUNCATE TABLE INSUMOS.DM_CAUSAS",
-#    schema="INSUMOS",
-#    table="DM_CAUSAS"
-#)
 
 load_dm_causas()
 load_dm_etiqueta_siniestro()
