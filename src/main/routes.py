3718b81a4a8fb18f4adfde23aac00bdfc94c56ff
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-27 15:36:58 -0600

    asegurando unicidad pagos proveedores

--- src/main/routes.py
+++ src/main/routes.py
@@ -2,9 +2,18 @@ from flask import Blueprint, jsonify
 from dependency_injector.wiring import inject, Provide
 
 blueprint = Blueprint('main_routes', __name__)
+
 @blueprint.route("/", methods=["GET"])
 @inject
 def root():
+  return {
+    "message": "It works!"
+  }
+
+
+@blueprint.route("/test", methods=["GET"])
+@inject
+def test():
   return {
     "message": "It works!"
   }
