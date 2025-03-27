63cb36da4ba263f2376f6fdf2365825490abab02
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-27 11:04:50 -0600

    agregando ignore de logs

--- vm_config.sh
+++ vm_config.sh
@@ -29,3 +29,5 @@ export LD_LIBRARY_PATH=/opt/oracle/instantclient_23_7:$LD_LIBRARY_PATH
 export PATH=/opt/oracle/instantclient_23_7:$PATH
 
 sudo apt install nginx
+
+sudo apt install build-essential libssl-dev libffi-dev python3-setuptools
