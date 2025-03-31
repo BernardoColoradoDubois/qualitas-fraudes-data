9a844fd3b45bd149b9f7d324c0c1888a9194618f
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-31 12:18:06 -0600

    cambiando ruta raiz a ruta abierta

--- src/main/routes.py
+++ src/main/routes.py
@@ -5,20 +5,10 @@ from src.lib.password_encrypt import APIKeyValidator
 
 blueprint = Blueprint('main_routes', __name__)
 
+
 @blueprint.route("/", methods=["GET"])
 @inject
-def root(api_key_validator:APIKeyValidator = Provide[DIContainer.api_key_validator]):
-  
-  if api_key_validator.validate(request.headers.get('Authorization').replace('Bearer ', '')) == False:
-    return jsonify({'message':'Forbidden'}), 403, {'ContentType':'application/json'}
-
-  return {
-    "msg": 'welcome'
-  }
-
-@blueprint.route("/test", methods=["GET"])
-@inject
-def test():
+def root():
   
   return {
     "msg": 'OK'
