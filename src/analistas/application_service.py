9fac9d845cc5098cd08787ab4f6090c77b302b8c
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-01 12:53:44 -0600

    agregando cascarones de servicios de aplicacion

--- src/analistas/application_service.py
+++ src/analistas/application_service.py
@@ -2,7 +2,7 @@ from src.lib.bigquery_to_oracle import BigQueryToOracle
 
 class LoadAnalistas:
   def __init__(self,bigquery_to_oracle: BigQueryToOracle):
-    self.bigquery_to_oracle = BigQueryToOracle()
+    self.bigquery_to_oracle = bigquery_to_oracle
 
   def invoque(self):
     ...
