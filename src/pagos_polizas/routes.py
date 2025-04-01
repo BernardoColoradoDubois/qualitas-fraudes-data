3346a927404a105a4f81cb283985ff147a919e0f
berni-rocket <bernardo.colorado@therocketcode.com>
2025-03-31 19:36:22 -0600

    agregando pagos polizas

---
+++ src/pagos_polizas/routes.py
@@ -0,0 +1,23 @@
+from flask import Blueprint, jsonify, request
+from dependency_injector.wiring import inject, Provide
+from src.lib.bigquery_to_oracle import BigQueryToOracle
+from src.lib.password_encrypt import APIKeyValidator
+from src.main.container import DIContainer
+
+blueprint = Blueprint('pagos_polizas_routes', __name__)
+
+@blueprint.route("/", methods=["POST"])
+@inject
+def load_pagos_polizas(api_key_validator:APIKeyValidator = Provide[DIContainer.api_key_validator],bigquery_to_oracle: BigQueryToOracle = Provide[DIContainer.bigquery_to_oracle]):
+  
+  if api_key_validator.validate(request.headers.get('Authorization').replace('Bearer ', '')) == False:
+    return jsonify({'message':'Forbidden'}), 403, {'ContentType':'application/json'}
+  
+  response = bigquery_to_oracle.run(
+    extraction_query="SELECT * FROM `qualitasfraude.DM_FRAUDES.DM_PAGOS_POLIZAS` ORDER BY ID", 
+    preload_query="TRUNCATE TABLE INSUMOS.DM_PAGOS_POLIZAS",
+    schema="INSUMOS",
+    table="DM_PAGOS_POLIZAS"
+  )   
+  
+  return jsonify(response), 200, {'ContentType':'application/json'}
