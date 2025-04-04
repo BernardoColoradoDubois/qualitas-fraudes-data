38f579fcb3abfd3a0dd605c6a42d335fccb6b164
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-04 11:02:39 -0600

    ultimos cambias en tablas insumo

--- dags/lib/qualitas_fraudes.py
+++ dags/lib/qualitas_fraudes.py
@@ -21,3 +21,13 @@ def load_api_data_by_date_range(url,date_generator_task_id,api_key,**kwargs):
   }
   response = requests.post(url, headers=headers, json=date_range)  
   print(response.json())
+
+
+def load_api_data(url,api_key,**kwargs):
+
+  headers = {
+    'Authorization': f'Bearer {api_key}',
+    'Content-Type': 'application/json'
+  }
+  response = requests.post(url, headers=headers)  
+  print(response.json())
