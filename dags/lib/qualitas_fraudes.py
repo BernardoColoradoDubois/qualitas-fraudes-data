31fe9b9fe44aba431dd05825595a956d4ddf4160
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-04 10:30:01 -0600

    ultimos cambias en tablas insumo

--- dags/lib/qualitas_fraudes.py
+++ dags/lib/qualitas_fraudes.py
@@ -1,7 +1,7 @@
 import requests
 from datetime import datetime, timedelta
 
-def date_interval_generator(init_date, final_date, **kwarg):
+def date_interval_generator(init_date, final_date, **kwargs):
 
   date_range= {
     'init-date': init_date,
@@ -11,10 +11,13 @@ def date_interval_generator(init_date, final_date, **kwarg):
   print(date_range)
   return date_range
 
-def load_api_data_by_date_range(url,date_generator_task_id,api_key,**context):
+def load_api_data_by_date_range(url,date_generator_task_id,api_key,**kwargs):
 
-  date_range = context['ti'].xcom_pull(task_ids=date_generator_task_id)
-  print(f"Date range from {date_range}")
-    
-  #response = requests.post(url, headers={"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"})
+  date_range = kwargs['ti'].xcom_pull(task_ids=date_generator_task_id)
 
+  headers = {
+    'Authorization': f'Bearer {api_key}',
+    'Content-Type': 'application/json'
+  }
+  response = requests.post(url, headers=headers, json=date_range)  
+  print(response.json())
