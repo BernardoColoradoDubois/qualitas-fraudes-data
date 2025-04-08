72533b4ae288c58b5c17a81ae74b19bcf5a469dc
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-08 13:54:31 -0600

    agregando rutas a app.py

--- app.py
+++ app.py
@@ -5,16 +5,19 @@ import os
 
 # Importar rutas
 import src.main.routes as main_routes
+import src.analistas.routes as analistas_routes
+import src.asegurados.routes as asegurados_routes
 import src.causas.routes as causas_routes
+import src.estados.routes as estados_routes
+import src.etiqueta_siniestro.routes as etiqueta_siniestro_routes
 import src.proveedores.routes as proveedores_routes
 import src.oficinas.routes as oficinas_routes
 import src.pagos_proveedores.routes as pagos_proveedores_routes
 import src.coberturas_movimientos.routes as coberturas_movimientos_routes
-import src.analistas.routes as analistas_routes
 import src.registro.routes as registro_routes
 import src.polizas_vigentes.routes as polizas_vigentes_routes
 import src.siniestros.routes as siniestros_routes
-import src.etiqueta_siniestro.routes as etiqueta_siniestro_routes
+import src.tipos_proveedores.routes as tipos_proveedores_routes
 import src.pagos_polizas.routes as pagos_polizas_routes
 
 # Importar middleware
@@ -44,8 +47,10 @@ container.config.hashed_api_key.override(hashed_api_key)
 container.wire(modules=[
   main_routes
   ,analistas_routes
+  ,asegurados_routes
   ,causas_routes
   ,coberturas_movimientos_routes
+  ,estados_routes
   ,etiqueta_siniestro_routes
   ,oficinas_routes
   ,proveedores_routes
@@ -54,6 +59,7 @@ container.wire(modules=[
   ,polizas_vigentes_routes
   ,registro_routes
   ,siniestros_routes
+  ,tipos_proveedores_routes
   ,middleware
 ])
 
@@ -61,8 +67,10 @@ container.wire(modules=[
 app = Flask(__name__)
 app.register_blueprint(main_routes.blueprint, url_prefix='/')
 app.register_blueprint(analistas_routes.blueprint, url_prefix='/analistas')
+app.register_blueprint(asegurados_routes.blueprint, url_prefix='/asegurados')
 app.register_blueprint(causas_routes.blueprint, url_prefix='/causas')
 app.register_blueprint(coberturas_movimientos_routes.blueprint, url_prefix='/coberturas-movimientos')
+app.register_blueprint(estados_routes.blueprint, url_prefix='/estados')
 app.register_blueprint(etiqueta_siniestro_routes.blueprint, url_prefix='/etiqueta-siniestro')
 app.register_blueprint(oficinas_routes.blueprint, url_prefix='/oficinas')
 app.register_blueprint(pagos_polizas_routes.blueprint, url_prefix='/pagos-polizas')
@@ -70,6 +78,7 @@ app.register_blueprint(pagos_proveedores_routes.blueprint, url_prefix='/pagos-pr
 app.register_blueprint(polizas_vigentes_routes.blueprint, url_prefix='/polizas-vigentes')
 app.register_blueprint(proveedores_routes.blueprint, url_prefix='/proveedores')
 app.register_blueprint(registro_routes.blueprint, url_prefix='/registro')
+app.register_blueprint(tipos_proveedores_routes.blueprint, url_prefix='/tipos-proveedores')
 app.register_blueprint(siniestros_routes.blueprint, url_prefix='/siniestros')
 
 # Solo ejecutar el servidor si se llama directamente
