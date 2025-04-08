245fb3c900157d38864718f68d34f3509006251e
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-08 13:44:10 -0600

    eliminando dependencias no necesarias de rutas

--- src/causas/routes.py
+++ src/causas/routes.py
@@ -1,7 +1,5 @@
 from flask import Blueprint, jsonify, request
 from dependency_injector.wiring import inject, Provide
-from src.lib.bigquery_to_oracle import BigQueryToOracle
-from src.lib.password_encrypt import APIKeyValidator
 from src.main.container import DIContainer
 from src.lib.middleware import token_required
 from src.causas.application_service import LoadCausas
