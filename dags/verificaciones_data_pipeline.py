0130f4f16ee393d0d16674e3d2a3aa91ba931a16
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-11 16:47:29 -0600

    armando dag de data fusion

---
+++ dags/verificaciones_data_pipeline.py
@@ -0,0 +1,49 @@
+import airflow
+from airflow import DAG
+from airflow.decorators import task
+from airflow.operators.bash import BashOperator
+from datetime import timedelta
+from airflow.operators.python import PythonOperator
+
+from airflow.providers.google.cloud.operators.datafusion import CloudDataFusionStartPipelineOperator
+from airflow.providers.google.cloud.operators.datafusion import CloudDataFusionGetInstanceOperator
+from airflow.providers.google.cloud.sensors.datafusion import CloudDataFusionPipelineStateSensor
+from airflow.providers.google.cloud.operators.datafusion import DataFusionPipelineType 
+
+default_args = {
+  'start_date': airflow.utils.dates.days_ago(0),
+  'retries': 1,
+  'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+  'verificaciones_data_pipeline',
+  default_args=default_args,
+  description='liveness monitoring dag',
+  schedule_interval='0 0 1 1 *',
+  max_active_runs=2,
+  catchup=False,
+  dagrun_timeout=timedelta(minutes=10),
+)
+
+init_landing = BashOperator(
+  task_id='init_landing',
+  bash_command='echo init landing',
+  dag=dag,
+)
+
+get_datafusion_instance = CloudDataFusionGetInstanceOperator(
+  task_id="get_datafusion_instance",
+  location='LOCATION',
+  instance_name='INSTANCE_NAME',
+  project_id='PROJECT_ID',
+  dag=dag,
+)
+
+end_landing = BashOperator(
+  task_id='end_landing',
+  bash_command='echo end landing',
+  dag=dag,
+)
+
+init_landing >> get_datafusion_instance
