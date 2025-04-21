e99477db5ec126b6640b4d09de3999a8b99ca89b
GustavoBorges <gustavo.borges@therocketcode.com>
2025-04-21 15:54:31 -0600

    Ajustando query para tabla destino

--- workspaces/models/SINIESTROS/DELETE_SINIESTROS.sql
+++ workspaces/models/SINIESTROS/DELETE_SINIESTROS.sql
@@ -1,3 +1,3 @@
 DELETE *
-FROM `{{task.params.SOURCE_PROJECT_ID}}.{{task.params.SOURCE_DATASET_NAME}}.{{task.params.SOURCE_TABLE_NAME}}`
+FROM `{{task.params.DEST_PROJECT_ID}}.{{task.params.DEST_DATASET_NAME}}.{{task.params.DEST_TABLE_NAME}}`
 WHERE CAST(FECHA_REGISTRO AS DATE) BETWEEN `{{task.params.init_date}}` AND `{{task.params.final_date}}`;
