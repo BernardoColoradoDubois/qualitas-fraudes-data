01ff1503a904548cac036309a9cdb994ba512424
GustavoBorges <gustavo.borges@therocketcode.com>
2025-04-21 15:31:57 -0600

    Agregando query para deletar los datos

---
+++ workspaces/models/REGISTRO/DELETE_REGISTRO.sql
@@ -0,0 +1,3 @@
+DELETE *
+FROM `{{task.params.SOURCE_PROJECT_ID}}.{{task.params.SOURCE_DATASET_NAME}}.{{task.params.SOURCE_TABLE_NAME}}`
+WHERE CAST(FECHA_ASIGNACION AS DATE) BETWEEN `{{task.params.init_date}}` AND `{{task.params.final_date}}`;
