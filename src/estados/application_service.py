72c43f31bdbc6a16961aee57c7a8787c71676a83
berni-rocket <bernardo.colorado@therocketcode.com>
2025-04-22 16:50:14 -0600

    ajustando cambio

--- src/estados/application_service.py
+++ src/estados/application_service.py
@@ -10,7 +10,7 @@ class LoadEstados:
     
     response = self.bigquery_to_oracle.run(
       extraction_query="SELECT * FROM `DM_FRAUDES.DM_ESTADOS`;", 
-      preload_query="DELETE FROM INSUMOS.DM_ESTADOS WHERE 1=1;",
+      preload_query="DELETE FROM INSUMOS.DM_ESTADOS WHERE 1=1 ;",
       schema="INSUMOS",
       table="DM_ESTADOS"
     )    
