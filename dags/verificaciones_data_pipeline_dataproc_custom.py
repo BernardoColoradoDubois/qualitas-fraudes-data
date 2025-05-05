8f26006c055f1efd66c137f52f9a126912466e41
berni-rocket <bernardo.colorado@therocketcode.com>
2025-05-05 11:44:53 -0600

    probando dag con cluster

--- dags/verificaciones_data_pipeline_dataproc_custom.py
+++ dags/verificaciones_data_pipeline_dataproc_custom.py
@@ -26,23 +26,37 @@ CLUSTER_CONFIG = {
       "enable_secure_boot": False,
       "enable_vtpm": False,
       "enable_integrity_monitoring": False,
-    },
+    }
   },
   "master_config": {
     "num_instances": 1,
     "machine_type_uri": "e2-custom-2-8192",
-    "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
+    "disk_config": {
+      "boot_disk_type": "pd-standard", "boot_disk_size_gb": 32
+    }
   },
   "worker_config": {
-    "num_instances": 30,
+    "num_instances": 36,
      "machine_type_uri": "e2-custom-2-8192",
-    "disk_config": {"boot_disk_type": "pd-standard", "boot_disk_size_gb": 32},
+    "disk_config": {
+      "boot_disk_type": "pd-standard", "boot_disk_size_gb": 32
+    }
+  },
+  "secondary_worker_config": {
+    "num_instances": 12,
+    "machine_type_uri": "e2-custom-2-8192",
+    "disk_config": {
+      "boot_disk_type": "pd-standard",
+      "boot_disk_size_gb": 32,
+    },
+    "is_preemptible": False,
+    "preemptibility": "PREEMPTIBLE",
   },
   "software_config": {
     "image_version":"2.1.85-debian11",
     "properties": {
       "dataproc:dataproc.conscrypt.provider.enable": "false",
-      #"capacity-scheduler:yarn.scheduler.capacity.resource-calculator":"org.apache.hadoop.yarn.util.resource.DefaultResourceCalculator",
+      "capacity-scheduler:yarn.scheduler.capacity.resource-calculator":"org.apache.hadoop.yarn.util.resource.DefaultResourceCalculator",
       "spark:spark.executor.cores": "10"
     }
   }
@@ -54,8 +68,8 @@ final_date = '2025-03-31'
 
 default_args = {
   'start_date': airflow.utils.dates.days_ago(0),
-  'retries': 3,
-  'retry_delay': timedelta(minutes=2)
+  'retries': 4,
+  'retry_delay': timedelta(minutes=5)
 }
 
 dag = DAG(
