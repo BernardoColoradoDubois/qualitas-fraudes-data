40732851441911baf9bbcefd77c63deab0b79968
berni-rocket <bernardo.colorado@therocketcode.com>
2025-05-13 12:59:15 -0600

    revisando cambios

--- workspaces/models/INCISOS_POLIZAS/DELETE_INCISOS_POLIZAS.sql
+++
@@ -1,3 +0,0 @@
-DELETE *
-FROM `{{task.params.DEST_PROJECT_ID}}.{{task.params.DEST_DATASET_NAME}}.{{task.params.DEST_TABLE_NAME}}`
-WHERE CAST(FECHA_CARGA AS DATE) BETWEEN `{{task.params.init_date}}` AND `{{task.params.final_date}}`;
