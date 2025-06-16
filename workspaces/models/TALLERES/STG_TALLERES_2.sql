ceeba8140d4e30889714e6ee7145d619a9a5bb8f
berni-rocket <bernardo.colorado@therocketcode.com>
2025-06-16 17:44:51 -0600

    creando sqls para elts de valuaciones

--- workspaces/models/TALLERES/STG_TALLERES_2.sql
+++ workspaces/models/TALLERES/STG_TALLERES_2.sql
@@ -1,3 +1,5 @@
+TRUNCATE TABLE `{{task.params.DEST_PROJECT_ID}}.{{task.params.DEST_DATASET_NAME}}.{{task.params.DEST_TABLE_NAME}}`;
+INSERT INTO `{{task.params.DEST_PROJECT_ID}}.{{task.params.DEST_DATASET_NAME}}.{{task.params.DEST_TABLE_NAME}}`
 SELECT 
   IDTALLER,
   IDOFICINA,
@@ -54,5 +56,5 @@ SELECT
   FECACEPTAAVISOPO,
   IDTIPOTALLER,
   ACTIVAXNUUP
-FROM `qlts-dev-mx-au-bro-verificacio.LAN_VERIFICACIONES.TALLERES`;
-WHERE RN = 1
+FROM `{{task.params.SOURCE_PROJECT_ID}}.{{task.params.SOURCE_DATASET_NAME}}.{{task.params.SOURCE_TABLE_NAME}}`
+WHERE RN = 1;
