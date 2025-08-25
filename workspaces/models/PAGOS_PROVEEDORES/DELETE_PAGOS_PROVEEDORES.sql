17475f797e63171ceae8e69c2d98d0edb3e8ad46
berni-rocket <bernardo.colorado@therocketcode.com>
2025-08-25 16:46:03 -0600

    renombrando tablas en el pipeline principal

--- workspaces/models/PAGOS_PROVEEDORES/DELETE_PAGOS_PROVEEDORES.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM `{{task.params.DEST_PROJECT_ID}}.{{task.params.DEST_DATASET_NAME}}.{{task.params.DEST_TABLE_NAME}}` 
-WHERE CAST(FECHA_PAGO AS DATE) BETWEEN '{{task.params.init_date}}' AND '{{task.params.final_date}}';
