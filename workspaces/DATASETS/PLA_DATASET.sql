4941eef48fac5501a88b5cae5d67b7435e5514ab
SaulCordova <saul.hernandez@therocketcode.com>
2025-08-28 13:13:46 -0600

    Rename variable airflow

---
+++ workspaces/DATASETS/PLA_DATASET.sql
@@ -0,0 +1,5 @@
+CREATE SCHEMA IF NOT EXISTS `{{task.params.PROJECT_ID}}.{{task.params.DATASET_NAME}}`
+OPTIONS (
+  location = `{{task.params.LOCATION}}`,
+  description = 'Este es el nuevo dataset para capa bronce.'
+);
